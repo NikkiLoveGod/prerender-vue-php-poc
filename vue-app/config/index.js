d4ed53874f70cdceb1959427eb1bb3ebe23ab086
Sami Kurvinen <sami.kurvinen@gmail.com>
2019-01-03 13:46:01 +0200

    Fixes public path

--- vue-app/config/index.js
+++ vue-app/config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../../php-app/vue-dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/vue-dist/',
 
     /**
      * Source Maps
