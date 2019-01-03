48870b7b2271b1e24cf33ad0c70f32fbbfafb79c
Sami Kurvinen <sami.kurvinen@gmail.com>
2019-01-03 16:11:25 +0200

    Adds memory cache

--- prerenderer/index.js
+++ prerenderer/index.js
@@ -1,4 +1,8 @@
 const prerender = require('prerender');
+const memoryCache = require('prerender-memory-cache')
 const server = prerender();
+
 server.use(prerender.removeScriptTags())
+server.use(memoryCache)
+
 server.start();
